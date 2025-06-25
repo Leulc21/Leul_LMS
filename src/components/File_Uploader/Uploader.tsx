75c1087da4b5d108869cb16d63675132661bb8d5
Leulc21 <leulchanie576@gmail.com>
2025-06-25 22:53:36 +0300

    file rejection logic coded

--- src/components/File_Uploader/Uploader.tsx
+++ src/components/File_Uploader/Uploader.tsx
@@ -1,7 +1,9 @@
 "use client";
+
 import { cn } from "@/lib/utils";
 import { useCallback } from "react";
-import { useDropzone } from "react-dropzone";
+import { FileRejection, useDropzone } from "react-dropzone";
+import { toast } from "sonner";
 import { Card, CardContent } from "../ui/card";
 import { Rendered_State } from "./Rendered_State";
 
@@ -9,7 +11,39 @@ function Uploader() {
   const onDrop = useCallback((acceptedFiles: File[]) => {
     console.log(acceptedFiles);
   }, []);
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  function Rejected(fileRejection: FileRejection[]) {
+    if (fileRejection.length > 0) {
+      const ToManyFile = fileRejection.find(
+        (fileRejection) => fileRejection.errors[0].code === "too-many-files"
+      );
+      const FileTooLarge = fileRejection.find(
+        (fileRejection) => fileRejection.errors[0].code === "file-too-large"
+      );
+      const InvalidType = fileRejection.find(
+        (fileRejection) => fileRejection.errors[0].code === "file-invalid-type"
+      );
+      if (InvalidType) {
+        toast.error("❌ Invalid file type. Only image files are allowed");
+      }
+      if (FileTooLarge) {
+        toast.error("File is too large, maximum size is 5MB");
+      }
+      if (ToManyFile) {
+        toast.error("You can only upload one file at a time");
+      }
+    }
+  }
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: { "image/*": [] },
+    maxFiles: 1,
+    maxSize: 5 * 1024 * 1024, // 5 MB
+    multiple: false,
+    onDropRejected: Rejected,
+  });
+
   return (
     <Card
       {...getRootProps()}
@@ -20,14 +54,10 @@ function Uploader() {
           : "border-border hover:border-primary"
       )}
     >
-      <CardContent className="flex  items-center justify-center h-full w-full">
+      <CardContent className="flex items-center justify-center h-full w-full">
         <input {...getInputProps()} />
-        {isDragActive ? (
-          <p>Drop the files here ...</p>
-        ) : (
-          <Rendered_State isDragActive={isDragActive} />
-          //   <Rendered_Error />
-        )}
+        <Rendered_State isDragActive={isDragActive} />
+        {/* <Rendered_Error /> */}
       </CardContent>
     </Card>
   );
